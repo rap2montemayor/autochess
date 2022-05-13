e40a3674e0dc6dbcfc3a8979e14b5e1d93cf5ad7
Kyusune <98139618+Kyusune@users.noreply.github.com>
2022-05-13 19:06:55 +0800

    Visuals and Bug
    Added tilemaps to WorldArea01 and WorldArea02 Scenes
    Updated Title Screen
    Fixed a bug when moving to WorldArea01 from WorldArea02

--- Assets/Scripts/Overworld/DialogueOnStart.cs
+++ Assets/Scripts/Overworld/DialogueOnStart.cs
@@ -7,8 +7,18 @@ using UnityEngine.Events;
 public class DialogueOnStart : MonoBehaviour
 {
     public string inkStoryDialogue;
+<<<<<<< Updated upstream
     void Start()
     {
         Dialogue.StartDialogue(inkStoryDialogue);
+=======
+    static bool doOnce = false;
+    void Start()
+    {
+        if(!doOnce){
+            Dialogue.StartDialogue(inkStoryDialogue);
+            doOnce = true;
+        }
+>>>>>>> Stashed changes
     }
 }
